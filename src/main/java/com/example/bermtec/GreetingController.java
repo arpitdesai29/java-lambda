fd6209857aa63f1a9a9a1cc436ac35ec9731e102
Arpit Desai <arpit5000@gmail.com>
2022-11-23 14:56:45 -0500

    sadsa

--- src/main/java/com/example/bermtec/GreetingController.java
+++ src/main/java/com/example/bermtec/GreetingController.java
@@ -2,7 +2,9 @@ package com.example.bermtec;
 
 
 public class GreetingController {
-    public String greeting(String input) {
-        return "Please Welcome Aboard " + input;
+   // public String greeting(String input) {
+    public String greeting() {
+       // return "Please Welcome Aboard " + input;
+        return "Please Welcome Aboard ";
     }
 }
