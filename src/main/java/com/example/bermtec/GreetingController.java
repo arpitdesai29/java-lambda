e688c1fc83d84027422e63ff63978c4db4d3d050
Arpit Desai <arpit5000@gmail.com>
2022-11-23 14:51:53 -0500

    adas

--- src/main/java/com/example/bermtec/GreetingController.java
+++ src/main/java/com/example/bermtec/GreetingController.java
@@ -2,7 +2,7 @@ package com.example.bermtec;
 
 
 public class GreetingController {
-    public String greeting(string input) {
+    public String greeting(String input) {
         return "Please Welcome Aboard " + input;
     }
 }
